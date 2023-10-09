58676a1b4eab21c7c50dc03141ea2309ca04e4f5
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-10-09 11:20:35 +0300

    Implement the project throttling

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -136,6 +136,14 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "100/day",
+        "user": "1000/day",
+    },
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
