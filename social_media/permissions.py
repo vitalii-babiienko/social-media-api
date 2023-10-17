ce4562902af0cecba6902795d7567b93fa06e438
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-10-17 15:30:35 +0300

    Improve code style into permission classes

--- social_media/permissions.py
+++ social_media/permissions.py
@@ -3,13 +3,15 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class IsProfileOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-        return obj.user == request.user
+        return (
+            True if request.method in SAFE_METHODS
+            else obj.user == request.user
+        )
 
 
 class IsPostOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-        return obj.author == request.user.profile
+        return (
+            True if request.method in SAFE_METHODS
+            else obj.author == request.user.profile
+        )
